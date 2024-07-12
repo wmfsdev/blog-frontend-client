4509b14eaac60cce2b72e5e5ffb76a62fe31f3aa
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-07-12 23:35:23 +0100

    add Body component for rendering dedicated Signup and Login forms respectively

---
+++ src/components/Body.jsx
@@ -0,0 +1,22 @@
+import { useParams } from "react-router-dom";
+import Login from "./Login";
+import Signup from "./Signup";
+
+const Body = () => {
+    const { name } = useParams();
+  
+    return (
+      <>
+        
+        {name === "signup" ? (
+          <Signup />
+        ) : name === "login" ? (
+          <Login />
+        ) : (
+          <DefaultProfile />
+        )}
+      </>
+    );
+};
+
+export default Body
