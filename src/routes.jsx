12b8339a972e0477db30b1540e7358927d81579c
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-07-12 23:28:27 +0100

    add new child paths for Body, Article and Articles components

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,15 +1,16 @@
 import App from "./App"
 import Body from "./components/Body"
+import Articles from "./components/Articles"
+import Article from "./components/Article"
 
 const routes = [
     {
         path: "/",
         element: <App />,
         children: [
-            {
-                path: "/:name",
-                element: <Body />
-            }
+            { path: "/:name", element: <Body /> },
+            { path: "/", element: <Articles /> },
+            { path: "/article/:id", element: <Article /> }
         ]
     },
 ]
