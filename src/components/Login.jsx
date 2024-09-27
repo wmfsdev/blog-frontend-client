7eb39d56eebcbd529d03291083be6fc6e4fc1837
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-09-27 23:53:44 +0100

    add submit handler (POST request) for login form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 const Login = () => {
     
     // authenticates - expect server response with signed token
     // set token as header auth in local storage
     const [ error, setError ] = useState(false)
+    const navigate = useNavigate()
 
     function handleSubmit(e) {
         e.preventDefault()
@@ -16,7 +18,8 @@ const Login = () => {
 
     async function login(username, password) {
         try {
-            const response = await fetch('http://localhost:3000/signin', {
+            console.log("login click attempt")
+            const response = await fetch('http://localhost:4000/login-form', {
                 method: "POST",
                 body: JSON.stringify({
                     username: username,
@@ -26,18 +29,34 @@ const Login = () => {
                     'Content-type': 'application/json; charset=UTF-8',
                 },
             })
-
+            // unauthenticated
+            if (response.status === 401) {
+                console.log("client unauthenticated")
+                const unauth = await response.json()
+                console.log("unauth response", unauth)
+                // set unauthorised error message
+                setError(unauth[0].message)
+                return
+            }
             if (response.status === 200) {
                 const token = await response.json()
+                console.log("react token log", token)
                 const key = Object.keys(token)
                 const value = Object.values(token)
                 localStorage.setItem(key, value)
-                // redirect
+                console.log("success: login")
+                navigate("/")
             } else {
-                const errMessage = await response.json()
-                setError(errMessage) 
+                // validation error handling
+                const errors = await response.json()
+                const status = await response.status
+                console.log("validation errors", status, errors)
+                // set validation error message
+                setError(errors[0].msg)
             }
         } catch(err) {
+            // probably should some generic error status/message... 500?
+            console.log("catch err")
             console.log(err)
         }
     }
@@ -47,12 +66,13 @@ const Login = () => {
             <h1>Login</h1>
             <form method="post" onSubmit={handleSubmit}>
                 <label htmlFor="username">Username
-                <input id="username" name="username" type="text" required={true} /></label>
+                <input name="username" type="text" required={true} /></label>
                 <label htmlFor="password">Password
-                <input id="password" name="password" type="password" required={true} /></label>
+                <input name="password" type="password" required={true} /></label>
                 <button type="submit">submit</button>
             </form>
-            { error && <p>{error.message}</p> }
+            {/*need a way to distinguish between array of errors and non array*/}
+            { error && <p>{error}</p> }
         </div>    
     )
 }
