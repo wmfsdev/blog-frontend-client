9f2ade1f2dd844cd576c0eecf40fe24576167e69
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-09-27 23:59:51 +0100

    testing child paths in routing

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,6 +2,10 @@ import App from "./App"
 import Body from "./components/Body"
 import Articles from "./components/Articles"
 import Article from "./components/Article"
+import Protected from "./components/Protected"
+import { loader as protectLoader } from './util/loader'
+import { articlesLoader, articleLoader } from './util/loader'
+import Comments from "./components/Comments"
 
 const routes = [
     {
@@ -9,8 +13,26 @@ const routes = [
         element: <App />,
         children: [
             { path: "/:name", element: <Body /> },
-            { path: "/", element: <Articles /> },
-            { path: "/article/:id", element: <Article /> }
+            { path: "/", loader: articlesLoader, element: <Articles /> },
+            { 
+                path: "/article/:id",
+              //  loader: articleLoader,
+                element: <Article />,
+                children: [
+                    { 
+                        path:'/article/:id', 
+                       // loader: protectLoader, 
+                        //element: <Protected /> 
+                        element: <Comments />
+                    },
+                ]
+             
+            },
+            {
+                path: '/protected/:id',
+                loader: protectLoader,
+                element: <Protected />
+            }
         ]
     },
 ]
