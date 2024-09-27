19e57dedcd7da426fb46f2027ed76210a4d3208b
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-09-27 23:57:48 +0100

    add Link for root directory

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,11 +1,11 @@
-import { Link } from "react-router-dom"
-import { Outlet } from "react-router-dom"
+import { Link, Outlet } from "react-router-dom"
 
 const Navigation = () => {
 
     return (
       <>
       <div className="navigation">
+        <Link to="/">Home</Link>
         <Link to="login">Login</Link>
         <Link to="signup">Signup</Link>
       </div>
