43e6b448d1028b83349e1f563de3039afe913ed6
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-09-27 23:53:08 +0100

    add submit handler (POST request) for signup form

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,15 +1,60 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
 const Signup = () => {
+
+    const [ error, setError ] = useState(false)
+    const navigate = useNavigate()
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        const data = new FormData(e.target)
+        const username = data.get("username")
+        const password = data.get("password")
+        const confirmPwd = data.get("password_confirm")
+        console.log(username, password, confirmPwd)
+        signup(username, password, confirmPwd)
+    }
+
+    async function signup(username, password, confirmPwd) {
+
+        try {
+            const response = await fetch('http://localhost:4000/signup-form', {
+                method: "POST",
+                body: JSON.stringify({
+                    username: username,
+                    password: password,
+                    confirm: confirmPwd,
+                }),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                },
+            })
+
+            if (response.status === 200) {
+                const token = await response.json()
+                console.log("TOKEN: ", token)
+                const key = Object.keys(token)
+                const value = Object.values(token)
+                localStorage.setItem(key, value)
+                console.log("success: signup => login")
+                navigate("/")
+            }
+        } catch(err) {
+            console.log(err)
+        }
+    }
     
     return (
         <div className="signup">
             <h1>Signup</h1>
-            <form method="post" action="http://localhost:3000/signup">
+            <form method="post" onSubmit={handleSubmit}>
                 <label htmlFor="username">Username
                 <input id="username" name="username" type="text" /></label>
                 <label htmlFor="password">Password
                 <input id="password" name="password" type="password" required="true" /></label>
-                <label htmlFor="password-confirm">Confirm
-                <input id="password-confirm" name="password-confirm" type="password" /></label>
+                <label htmlFor="password_confirm">Confirm
+                <input id="password_confirm" name="password_confirm" type="password" /></label>
                 <button type="submit">submit</button>
             </form>
         </div>
