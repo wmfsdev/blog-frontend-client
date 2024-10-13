826fbd87b0c322030c4c33226db08dd559aa797c
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-10-13 23:04:14 +0100

    update vite config to use environment variables

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
+import { config } from 'dotenv'
 import react from '@vitejs/plugin-react'
+
+config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': process.env
+  }
 })
