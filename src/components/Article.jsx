e9d1e88c5d1c620149907af51f229fc2530d58fd
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-10-14 23:03:14 +0100

    use LoaderData in Article

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,23 +1,17 @@
-import { useLoaderData, useLocation, Outlet } from "react-router-dom"
+import { useLoaderData, Outlet } from "react-router-dom"
 
 const Article = () => {
-    // from Articles Link... how to re-render on state change?
-    
-    // maybe forget Link and have it use it's own dedicated Loader
-    // to make a request for just comments
 
-    const { state } = useLocation();
-    console.log("useLocation: ", state)
+    const article = useLoaderData()
+    console.log("react article", article)
 
         return (
             <>
             <div id="article">
-                <h1>{state.title}</h1>
-                <p>{state.body}</p>
-            
+                <h1>{article.title}</h1>
+                <p>{article.body}</p>
             </div>
-            <Outlet context={state.comments} />
-            {/*goes to Comments useOutlet*/}
+            <Outlet  />
             </>
         )
 }
