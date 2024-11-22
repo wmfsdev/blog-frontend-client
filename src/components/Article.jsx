3a102efcadd4df10a2232a842997467847d93129
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-11-22 20:24:49 +0000

    update div tag with className

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -4,15 +4,18 @@ import DOMPurify from 'dompurify'
 const Article = () => {
 
     const article = useLoaderData()[0]
+    console.log(article)
+   //console.log("string: ", article.body.at(5))
+    // console.log("non-string: ", "`" + article.body + "`")
 
         return (
-            <>
+            <div className="article-wrapper">
             <div id="article">
                 <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(article.title) }}/>
                 <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(article.body) }}/>
             </div>
             <Outlet  />
-            </>
+            </div>
         )
 }
 
