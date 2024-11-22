4106008159afd8028eea4ab6832c05bc2a4c0e1c
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-11-22 20:29:37 +0000

    add state to provide context to child components (in Outlet) with logOut function for toggling said state

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,18 +1,43 @@
 import { Link, Outlet } from "react-router-dom"
+import { useState } from "react";
 
 const Navigation = () => {
 
+  const token = localStorage.getItem("token");
+  const [ tokenState, setTokenState ] = useState(token)
+ 
+  function logOut() {
+    localStorage.clear()
+    setTokenState(null)
+  }
+    
+  if (tokenState === null) {
     return (
       <>
       <h1>BLOG</h1>
       <div className="navigation">
-        <Link to="/">HOME</Link>
+        <Link to="/">ARTICLES</Link>
         <Link to="login">LOGIN</Link>
         <Link to="signup">SIGNUP</Link>
       </div>
-      <Outlet />
+      <Outlet context={[ tokenState, setTokenState ]}/>
       </>
     )
+  } else return (
+    (
+    <>
+    <h1>BLOG</h1>
+    <div className="navigation">
+      <Link to="/">ARTICLES</Link>
+      <Link to="/" onClick={logOut}>LOGOUT</Link>
+    </div>
+    <Outlet context={[ tokenState, setTokenState ]}/>
+    </>
+    )
+  )
 }
+
+
+
 
 export default Navigation
