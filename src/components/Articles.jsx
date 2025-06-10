0234bb8d250161703a45a7feb12646784195b295
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-06-10 22:54:11 +0100

    import module for helper functions and add img element with src set to thumbnail url

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,26 +1,24 @@
 
 import { Link, useLoaderData } from 'react-router-dom'
+import { createDateStamp, strip } from '../util/helpers'
 
 const Articles = () => {
   
     const articles = useLoaderData()
-
-    function strip(html){
-       let doc = new DOMParser().parseFromString(html, 'text/html');
-       return doc.body.textContent || "";
-    }
-
+    console.log(articles)
+    
     return (
       <div className='articles'>
         {
           articles.map((article) => {
             return (
-              <Link key={article.id} to={`article/${article.id}`}>
-                <div className='article-post'>
+              <div className='article-post' key={article.id}>
+                <Link key={article.id} to={`article/${article.id}`}>
+                  <img src={article.thumbnail} alt="" />
                   <h2>{strip(article.title)}</h2>
-                  <p>{strip(article.body)}</p>
-                </div>
-              </Link>
+                  <p>{createDateStamp(article.timestamp)}</p>
+                </Link>
+              </div>
             )
           })
         }
